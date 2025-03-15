24b8f8ce10d2f7f4a558d095e6c99349316cbfe0
yunhyunjung <ms7257@naver.com>
2025-03-15 14:51:56 +0900

    python file

---
+++ python/keywordEx.py
@@ -0,0 +1,13 @@
+import keyword
+ 
+#ros is python entry point function need! 
+#because cpp is main class exists
+
+# function definiation
+def main():
+    print(keyword.kwlist)
+
+
+# module call 
+if __name__ == "__main__":
+    main()
---
+++ python/printVariable.py
@@ -0,0 +1,12 @@
+
+def main():
+    var_a = 100
+    print("variable a")
+    print(100+100)
+    print(100*100)
+    print(100-50)
+    print(var_a)
+    print("variable a :", var_a)
+
+if __name__ == "__main__":
+    main()
