7f8a8124ebc3f5171eefada114fedad4d3d27e41
yunhyunjung <ms7257@naver.com>
2025-04-05 15:47:14 +0900

    ddd

--- classEx.py
+++ classEx.py
@@ -8,19 +8,29 @@ class Student:
         self.math = math
         self.english = english
         self.sience =science
+        self.sum = 0
 
+    #get_sum Method
     def get_sum(self):
         return self.korean + self.math + self.english + self.sience
 
+    #get_average Method
     def get_average(self):
         return self.get_sum()/4
     
-
+    #get_calculated_sum Method
+    def get_calculated_sum(self):
+        return self.sum
+    
 def main():
     a = Student("choi", 10,20,30,40)
     b = Student("kim", 11,21,31,41)
     print(a,b)
     # print(a.sience, a.english, a.math, a.name)
- 
+    print(a.get_calculated_sum())   
+    print(a.get_average())
+
 if __name__ == "__main__":
     main()
+
+ 
