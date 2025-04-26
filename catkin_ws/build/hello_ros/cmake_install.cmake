270fb6813c2623ba76635e9e669dcf32be2ca68a
yunhyunjung <ms7257@naver.com>
2025-04-26 05:38:37 +0000

    service commit

--- catkin_ws/build/hello_ros/cmake_install.cmake
+++ catkin_ws/build/hello_ros/cmake_install.cmake
@@ -85,6 +85,10 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/hello_ros" TYPE PROGRAM FILES "/kuLimo/catkin_ws/build/hello_ros/catkin_generated/installspace/simpleServiceServer")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/hello_ros" TYPE DIRECTORY FILES "/kuLimo/catkin_ws/src/hello_ros/launch")
 endif()
 
